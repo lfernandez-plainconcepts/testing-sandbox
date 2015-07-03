f60dd949d13d9661a9aab80d9c249e7490399c97
Luis Fernández <lfernandez.plain@live.com>
2015-07-03 12:01:46 +0200

    Initial Commit
    Initial Commit with the solution

---
+++ WP8.1_MvvmLight_TestApp1/WP8.1_MvvmLight_TestApp1/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WP8._1_MvvmLight_TestApp1.Model;
+
+namespace WP8._1_MvvmLight_TestApp1.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
---
+++ WP8.1_MvvmLight_TestApp1/WP8.1_MvvmLight_TestApp1/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WP8._1_MvvmLight_TestApp1.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
